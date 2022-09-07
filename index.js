2021c1075d5661e00b0fba061b7b4b4098134f5a
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-07 07:06:42 +0100

    index.js updated // refactor

--- index.js
+++ index.js
@@ -1,40 +1,84 @@
-//  Imports:
-const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const Table = require("console.table");
-const Connection = require("mysql/lib/Connection");
-require("dotenv").config();
+const { prompt } = require("inquirer");
+const consoleTable = require("console.table");
 
-// Bring in the database connection
 const db = mysql.createConnection({
   host: "localhost",
+  port: 3306,
   user: "root",
   password: "",
-  database: "employee_db",
+  database: "employees_db",
 });
 
-db.connect((error) => {
-  if (error) throw error;
-  getStarted();
+db.connect(function (err) {
+  if (err) throw err;
+  console.table("Welcome to Employee Management System");
 });
 
-function getStarted() {
-  inquirer.prompt([
+// Questions:
+function letsStart() {
+  prompt([
     {
       type: "list",
-      name: "options",
-      message: "Welcome to the employee tracker! What would you like to do?",
+      name: "choice",
+      message: "What would you like to do?",
       choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a Role",
-        "Add an Employee",
-        "Update an Employee's Role",
+        {
+          name: "View all Departments",
+          value: "viewAllDepartments",
+        },
+        {
+          name: "View all Roles",
+          value: "viewAllRoles",
+        },
+        {
+          name: "View all Employees",
+          value: "viewAllEmployees",
+        },
+        {
+          name: "Add a new Department",
+          value: "addDepartment",
+        },
+        {
+          name: "Add a new Role",
+          value: "addRole",
+        },
+        {
+          name: "Add a new Employee",
+          value: "addEmployee",
+        },
+        {
+          name: "Update an existing Employee Role",
+          value: "updateEmployeeRole",
+        },
       ],
     },
-  ]);
+  ]).then((res) => {
+    let choice = res.choice;
+    switch (choice) {
+      case "viewAllDepartments":
+        viewAllDepartments();
+        break;
+      case "viewAllRoles":
+        viewAllRoles();
+        break;
+      case "viewAllEmployees":
+        viewAllEmployees();
+        break;
+      case "addDepartment":
+        addDepartment();
+        break;
+      case "addRole":
+        addNewRole();
+        break;
+      case "addEmployee":
+        addEmployee();
+        break;
+      case "updateEmployeeRole":
+        updateEmployeeRole();
+        break;
+    }
+  });
 }
 
-getStarted();
+letsStart();
