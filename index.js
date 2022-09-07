1dd5eb90b1a86cfa6d1a42ec87be17e20001bb52
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-07 04:56:16 +0100

    installed mysql2 // Started index.js

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+function getStarted() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "options",
+      message: "Welcome to the employee tracker! What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a Role",
+        "Add an Employee",
+        "Update an Employee's Role",
+      ],
+    },
+  ]);
+}
+
+getStarted();
